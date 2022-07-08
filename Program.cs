be61073a72cba618a192f12892563edf4e76712b
fullstackfurkan <furkanyilmaz870@gmail.com>
2022-07-08 12:54:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+int year = DateTime.Now.Year;
+
+
+switch (year)
+{
+    case 2023:
+        Console.WriteLine("2023 yılındasınız.");
+        break;
+
+    case 2022:
+        Console.WriteLine("2022 yılındasınız.");
+        break;
+
+    case 2021:
+        Console.WriteLine("2021 yılındasınız.");
+        break;
+}
+
+
+switch (year)
+{
+    case 2020:
+    case 2021:
+    case 2022:
+    case 2023:
+        Console.WriteLine("Son 4 yıllık bir zaman dilimi seçtiniz.");
+        break;
+
+}
+
+
+
+
+
+
+
+Console.ReadLine();
